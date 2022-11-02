b6109a54f6c535a6547258eca8b73dc2166d2323
Jiyas muhammad <jiyasom001@gmail.com>
2022-11-03 02:31:49 +0400

    jjjkjk

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,16 +1,17 @@
-import { FooterModule } from '../../shared/footer/footer.module';
 import { HomeComponent } from './home.component';
 
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './homer-outing.module';
-import { HeaderModule } from '../../shared/header/header.module';
+
 import { OfferModule } from './components/offer/offer.module';
 import { BoxrowModule } from './components/boxrow/boxrow.module';
 import { featuresModule } from './components/features/features.module';
 import { ProfieModule } from './components/profile/profile.module';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { HeaderModule } from '../layout/header/header.module';
+import { FooterModule } from '../layout/footer/footer.module';
 
 @NgModule({
   declarations: [HomeComponent],
--- src/app/shared/modal/model.module.ts
+++ src/app/shared/modal/model.module.ts
@@ -1,6 +1,3 @@
-import { FooterModule } from '../footer/footer.module';
-
-import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal.component';
